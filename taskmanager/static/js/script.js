e64f2b034d9323a020ada6f8a3845fe5e0c3d908
nddpj00 <danieljones625@btinternet.com>
2023-01-25 14:58:13 +0000

    sets up templates

---
+++ taskmanager/static/js/script.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // sidenav initialiazation
+    let sidenav = document.querySelectorAll('.sidenav');
+    M.Sidenav.init(sidenav);
+});
